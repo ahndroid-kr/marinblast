bce7d6abd9f3ba16ea4bc1d8f6b61c86b9f65b9a
ahndroid-kr <bskirrrt@gmail.com>
2026-06-23 01:20:02 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   // GitHub Pages 배포 시 './' 또는 '/repo-name/' 으로 변경
-  base: './',
+base: '/Marineblast/',
   build: {
     target: 'es2020',
     minify: 'esbuild',
